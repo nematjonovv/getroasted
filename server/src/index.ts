0b22a4cb5cb8d5aa87e8bc6ce42c989e54b012f4
Hikmatillo <hikmatillonematjonovv19@gmail.com>
2026-03-30 19:18:07 +0500

    cors 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://getroasted-today.vercel.app/"],
+  origin: ["http://localhost:3000", "https://getroasted-today.vercel.app"],
   credentials: true,
 }))
 
